8761d6e8335ab170106b83e275dca054269e3949
BHARATH <160884754+23PA1A05E0@users.noreply.github.com>
2024-08-06 11:23:59 +0530

    Create Reverse.py

---
+++ LISTS/Reverse.py
@@ -0,0 +1,12 @@
+l=[]
+n=int(input("Enter the No.of Element:"))
+for i in range(n):
+	ele=int(input("Enter the element:"))
+	l.append(ele)
+print(l)
+print("After Reversing")
+print(l[::-1])
+
+'''l.reverse()
+print("After Reversing")
+print(l)'''
