19ca7fa46607e4d18dee10ebd7cd19622f5763f6
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2024-08-27 12:23:14 +0530

    Create Rev_LIst.py

---
+++ FUNCTIONS/Rev_LIst.py
@@ -0,0 +1,17 @@
+def Rev(l):
+	n=len(l)
+	l2=[]
+	while(n>=0):
+		l2.append(l[n-1])
+		#print(l[n-1])
+		n-=1
+	return l2
+l=[]
+n=int(input("Enter the size of list"))
+for i in range(n):
+	ele=int(input())
+	l.append(ele)
+print(l)
+print("After reversing")
+#Rev(l)	
+print(Rev(l))	
