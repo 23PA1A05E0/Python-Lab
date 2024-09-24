e4ce454a140c93aaf597c1106a474d1d68cdc1fc
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2024-09-24 12:10:07 +0530

    Add files via upload

---
+++ FILES/CSV.py
@@ -0,0 +1,15 @@
+import csv
+
+data = [['s.no','name'],[1,'bharath'],[2,'balaji']]
+
+
+f = open('table.csv','w')
+w= csv.writer(f)
+w.writerows(data)
+f.close()
+
+f = open('table.csv','r')
+r = csv.reader(f)
+for row in r:
+    print(row)
+
