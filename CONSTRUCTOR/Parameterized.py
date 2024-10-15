844265146ac93049e19b91b6e2ca6493f9c59f48
BHARATH KUMAR REDDY <160884754+23PA1A05E0@users.noreply.github.com>
2024-10-15 11:26:19 +0530

    Create Parameterized.py

---
+++ CONSTRUCTOR/Parameterized.py
@@ -0,0 +1,9 @@
+#Parameterized Consructor
+print("\nParamerized Constructor")
+class Para:
+	def __init__(self,name,rollno):
+		print(name,"\n",rollno,sep = "")
+
+p = Para("Bharath","23PA1A05E0")		
+
+		
